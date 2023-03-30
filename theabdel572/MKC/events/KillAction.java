9c65af6d5bcd600d547ae3d67b9989259cbb1b7c
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-03-30 22:10:26 +0200

    Added an interface to manage kill events methods.

---
+++ theabdel572/MKC/events/KillAction.java
@@ -0,0 +1,10 @@
+package theabdel572.MKC.events;
+
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import theabdel572.MKC.MobTypes.MobType;
+
+public interface KillAction {
+	
+	void onKill(EntityDeathEvent e, MobType mob);
+}
