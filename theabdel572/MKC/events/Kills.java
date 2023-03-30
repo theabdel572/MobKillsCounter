35c8eb5d06c9735f14009ef635950146fc7a70b0
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-03-30 22:13:05 +0200

    Refactorized the whole class.

--- theabdel572/MKC/events/Kills.java
+++ theabdel572/MKC/events/Kills.java
@@ -1,6 +1,8 @@
 package theabdel572.MKC.events;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -13,6 +15,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import theabdel572.MKC.MKC;
+import theabdel572.MKC.MobKillsList;
+import theabdel572.MKC.MobTypes.MobType;
 
 public class Kills implements Listener {
 	private MKC plugin;
@@ -24,189 +28,107 @@ public class Kills implements Listener {
 
 	@EventHandler
 	public void killEvent(EntityDeathEvent e) {
-		
+
 		Player killer = e.getEntity().getKiller();
 		EntityType entity = e.getEntityType();
 		FileConfiguration config = plugin.getConfig();
 		List<String> worlds = config.getStringList("Worlds.enabled-worlds");
 		
 		
+		if (killer == null || killer.getType() != EntityType.PLAYER) {
+			return;
+		}
+
+		//Checking if the world is in the enabled worlds list.
 		for (int i = 0; i < worlds.size(); i++) {
-			if (killer == null || killer.getType() != EntityType.PLAYER) {
-				break;
-			}
-			
 			String world = killer.getWorld().getName();
-				
+
 			if (worlds.get(i).equals(world)) {
 				isWorldAtList = true;
 				break;
 			}
 			isWorldAtList = false;
 		}
-		
-		if (!isWorldAtList || killer == null || killer.getType() != EntityType.PLAYER) {
+
+		if (!isWorldAtList) {
 			return;
 		}
-			switch (entity) {
-			case ZOMBIE:
-				onKill(e, "zombies");
-				break;
-			case SKELETON:
-				onKill(e, "skeletons");
-				break;
-			case SPIDER:
-				onKill(e, "spiders");
-				break;
-			case CAVE_SPIDER:
-				onKill(e, "cave_spiders");
-				break;
-			case BLAZE:
-				onKill(e, "blazes");
-				break;
-			case CREEPER:
-				onKill(e, "creepers");
-				break;
-			case ENDER_DRAGON:
-				onKill(e, "ender_dragons");
-				break;
-			case ENDERMAN:
-				onKill(e, "endermans");
-				break;
-			case ENDERMITE:
-				onKill(e, "endermites");
-				break;
-			case BAT:
-				onKill(e, "bats");
-				break;
-			case CHICKEN:
-				onKill(e, "chickens");
-				break;
-			case COW:
-				onKill(e, "cows");
-				break;
-			case GHAST:
-				onKill(e, "ghasts");
-				break;
-			case GUARDIAN:
-				onKill(e, "guardians");
-				break;
-			case HORSE:
-				onKill(e, "horses");
-				break;
-			case IRON_GOLEM:
-				onKill(e, "iron_golems");
-				break;
-			case MAGMA_CUBE:
-				onKill(e, "magma_cubes");
-				break;
-			case MUSHROOM_COW:
-				onKill(e, "mushroom_cows");
-				break;
-			case OCELOT:
-				onKill(e, "ocelots");
-				break;
-			case PIG:
-				onKill(e, "pigs");
-				break;
-			case PIG_ZOMBIE:
-				onKill(e, "pig_zombies");
-				break;
-			case PLAYER:
-				onKill(e, "players");
-				break;
-			case RABBIT:
-				onKill(e, "rabbits");
-				break;
-			case SHEEP:
-				onKill(e, "sheeps");
-				break;
-			case SILVERFISH:
-				onKill(e, "silverfishs");
-				break;
-			case SLIME:
-				onKill(e, "slimes");
-				break;
-			case SNOWMAN:
-				onKill(e, "snowmans");
-				break;
-			case SQUID:
-				onKill(e, "squids");
-				break;
-			case VILLAGER:
-				onKill(e, "villagers");
-				break;
-			case WITCH:
-				onKill(e, "witchs");
-				break;
-			case WITHER:
-				onKill(e, "withers");
-				break;
-			case WOLF:
-				onKill(e, "wolfs");
-				break;
-			default:
-				break;
-			}
-	}
+		
+		Map<EntityType, KillAction> entityActions = new HashMap<EntityType, KillAction>();
+		
+		entityActions.put(entity, new KillAction() {
 
-	public void onKill(EntityDeathEvent e, String mob) {
-		Player killer = e.getEntity().getKiller();
-		FileConfiguration players = plugin.getPlayers();
-		FileConfiguration config = plugin.getConfig();
-		ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-		// System to add mobkills to players & rewards system.
-		if (players.contains("Players." + killer.getUniqueId() + "." + mob)) {
-			int counter = Integer.valueOf(players.getString("Players." + killer.getUniqueId() + "." + mob));
-			players.set("Players." + killer.getUniqueId() + "." + mob, counter + 1);
-			plugin.savePlayers();
-			List<String> rewardPath = config.getStringList("Rewards." + mob + "." + (counter + 1));
-			if (config.contains("Rewards." + mob + "." + (counter + 1))) {
-					for(int i = 0; i < rewardPath.size(); i++) {
-						String command = rewardPath.get(i);
-						if (config.getBoolean("Config.Send-Message-insteadof-Command")) {
-							if (command.startsWith("msg %player%")) {
-								killer.sendMessage(ChatColor
-										.translateAlternateColorCodes('&', command.replaceAll("msg %player%", "")).trim());
-							} else if (command.startsWith("tell %player%")) {
-								killer.sendMessage(ChatColor
-										.translateAlternateColorCodes('&', command.replaceAll("tell %player%", "")).trim());
-							} else {
-								Bukkit.dispatchCommand(console, ChatColor.translateAlternateColorCodes('&',
-										command.replaceAll("%player%", killer.getName())));
-							}
-						} else {
-							Bukkit.dispatchCommand(console, ChatColor.translateAlternateColorCodes('&',
-									command.replaceAll("%player%", killer.getName())));
-						}
-					}
-			}
-		} else {
-			players.set("Players." + killer.getUniqueId() + ".name", killer.getName());
-			players.set("Players." + killer.getUniqueId() + "." + mob, 1);
-			int counter = Integer.valueOf(players.getString("Players." + killer.getUniqueId() + "." + mob));
-			List<String> rewardPath = config.getStringList("Rewards." + mob + "." + counter);
-			plugin.savePlayers();
-			if (config.contains("Rewards." + mob + "." + counter)) {
-				for(int i = 0; i < rewardPath.size(); i++) {
-					String command = rewardPath.get(i);
-					if (config.getBoolean("Config.Send-Message-insteadof-Command")) {
-						if (command.startsWith("msg %player%")) {
-							killer.sendMessage(ChatColor
-									.translateAlternateColorCodes('&', command.replaceAll("msg %player%", "")).trim());
-						} else if (command.startsWith("tell %player%")) {
-							killer.sendMessage(ChatColor
-									.translateAlternateColorCodes('&', command.replaceAll("tell %player%", "")).trim());
-						} else {
-							Bukkit.dispatchCommand(console, ChatColor.translateAlternateColorCodes('&',
-									command.replaceAll("%player%", killer.getName())));
-						}
-					} else {
-						Bukkit.dispatchCommand(console, ChatColor.translateAlternateColorCodes('&',
-								command.replaceAll("%player%", killer.getName())));
-					}
+			@Override
+			public void onKill(EntityDeathEvent e, MobType mob) {
+				Player killer = e.getEntity().getKiller();
+				FileConfiguration players = plugin.getPlayers();
+				FileConfiguration config = plugin.getConfig();
+				ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+				String path = "Players." + killer.getUniqueId() + "." + mob.getPath();
+				
+				// System to add mobkills to players & rewards system.
+				
+				if(MobKillsList.hasMobKill(plugin, killer, mob)) {
+					
+					//Adding 1 kill to the counter.
+					int counter = players.getInt(path);
+					players.set(path, counter + 1);
+					
+					plugin.savePlayers();
+					
+					executeRewards(killer, mob);
+					return;
 				}
+				players.set("Players." + killer.getUniqueId() + ".name", killer.getName());
+				players.set("Players." + killer.getUniqueId() + "." + mob.getPath(), 1);
+				plugin.savePlayers();
+				
+				executeRewards(killer, mob);
+			}
+			
+		});
+		
+		entityActions.get(entity).onKill(e, MobType.getFromEntityType(entity));
+	}
+	
+	//Checking if the command is /tell or /msg.
+	public boolean isMsgCommand(String command) {
+		return command.startsWith("msg %player%") || command.startsWith("tell %player%");
+	}
+	
+	// Method to send messages instead of and msg or tell command.
+	public void sendCommandMessage(Player player, String command) {		
+		player.sendMessage(ChatColor
+				.translateAlternateColorCodes('&', command.replaceAll("msg %player%", "").replaceAll("tell %player%", "")).trim());
+	}
+	
+	// Method to give rewards to players.
+	public void executeRewards(Player player, MobType mob) {
+		
+		FileConfiguration config = plugin.getConfig();
+		FileConfiguration players = plugin.getPlayers();
+		String path = "Players." + player.getUniqueId() + "." + mob.getPath();
+		
+		int counter = players.getInt(path);
+		
+		List<String> rewardPath = config.getStringList("Rewards." + mob.getPath() + "." + counter);
+		//Checking if that kill number has a reward established.
+		if (!config.contains("Rewards." + mob.getPath() + "." + counter)) {
+			return;
+		}
+		
+		for (int i = 0; i < rewardPath.size(); i++) {
+			
+			String command = rewardPath.get(i);
+			//Checking if the command is /tell or /msg.
+			if (config.getBoolean("Config.Send-Message-insteadof-Command") && isMsgCommand(command)) {
+				sendCommandMessage(player, command);
+				
+			}else {
+				Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), ChatColor.translateAlternateColorCodes('&',
+						command.replaceAll("%player%", player.getName())));
 			}
 		}
+		return;
 	}
 }
