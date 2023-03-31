3a19dce11275a760b3acf6a8a49bb5b726a385b8
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-03-31 23:15:31 +0200

    Optimized this class a little bit. Refactor finished.

--- theabdel572/MKC/commands/Comands.java
+++ theabdel572/MKC/commands/Comands.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import theabdel572.MKC.MKC;
+import theabdel572.MKC.MobTypes.MobType;
 
 public class Comands implements CommandExecutor {
 	private MKC plugin;
@@ -16,37 +17,30 @@ public class Comands implements CommandExecutor {
 	public Comands(MKC plugin) {
 		this.plugin = plugin;
 	}
-
+	
+	// Well i'm lazy to refactor this class, i'm making just some changes.
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length > 0) {
 			FileConfiguration config = plugin.getConfig();
+			
 			if (args[0].equalsIgnoreCase("version")) {
+				
 				if (sender.hasPermission("mkc.version")) {
 					sender.sendMessage(plugin.getPluginName() + ChatColor.GREEN + " The plugin version is: "
 							+ ChatColor.BLUE + plugin.getVersion());
-				} else {
-					sender.sendMessage(plugin.getPluginName() + " " + ChatColor.translateAlternateColorCodes('&',
-							config.getString("Config.NoPermission-Message")));
+					return true;
 				}
+				
+				sender.sendMessage(plugin.getPluginName() + " " + ChatColor.translateAlternateColorCodes('&',
+						config.getString("Config.NoPermission-Message")));
+				
 			} else if (args[0].equalsIgnoreCase("setreward")) {
+				
 				if (sender.hasPermission("mkc.setrewards")) {
+					
 					if (args.length > 2) {
-						if (args[1].equalsIgnoreCase("zombies") || args[1].equalsIgnoreCase("skeletons")
-								|| args[1].equalsIgnoreCase("spiders") || args[1].equalsIgnoreCase("cave_spiders")
-								|| args[1].equalsIgnoreCase("blazes") || args[1].equalsIgnoreCase("creepers")
-								|| args[1].equalsIgnoreCase("ender_dragons") || args[1].equalsIgnoreCase("endermans")
-								|| args[1].equalsIgnoreCase("endermites") || args[1].equalsIgnoreCase("bats")
-								|| args[1].equalsIgnoreCase("chickens") || args[1].equalsIgnoreCase("cows")
-								|| args[1].equalsIgnoreCase("ghasts") || args[1].equalsIgnoreCase("guardians")
-								|| args[1].equalsIgnoreCase("horses") || args[1].equalsIgnoreCase("iron_golems")
-								|| args[1].equalsIgnoreCase("magma_cubes") || args[1].equalsIgnoreCase("mushroom_cows")
-								|| args[1].equalsIgnoreCase("ocelots") || args[1].equalsIgnoreCase("pigs")
-								|| args[1].equalsIgnoreCase("pig_zombies") || args[1].equalsIgnoreCase("players")
-								|| args[1].equalsIgnoreCase("rabbits") || args[1].equalsIgnoreCase("sheeps")
-								|| args[1].equalsIgnoreCase("silverfishs") || args[1].equalsIgnoreCase("slimes")
-								|| args[1].equalsIgnoreCase("snowmans") || args[1].equalsIgnoreCase("squids")
-								|| args[1].equalsIgnoreCase("villagers") || args[1].equalsIgnoreCase("witchs")
-								|| args[1].equalsIgnoreCase("withers") || args[1].equalsIgnoreCase("wolfs")) {
+						
+						if (MobType.isPath(args[1])) {
 							try {
 								int kills4reward = Integer.parseInt(args[2]);
 								List<String> rewards = new ArrayList<>();
@@ -78,9 +72,11 @@ public class Comands implements CommandExecutor {
 							config.getString("Config.NoPermission-Message")));
 				}
 			} else if (args[0].equalsIgnoreCase("reload")) {
+				
 				if (sender.hasPermission("mkc.reload")) {
 					plugin.reloadConfig();
 					sender.sendMessage(plugin.getPluginName() + ChatColor.GREEN + " The config has been reloaded.");
+					
 				} else {
 					sender.sendMessage(plugin.getPluginName() + " " + ChatColor.translateAlternateColorCodes('&',
 							config.getString("Config.NoPermission-Message")));
@@ -98,12 +94,15 @@ public class Comands implements CommandExecutor {
 						+ " See if a plugin has a new version (send message to it on join).");
 				sender.sendMessage(ChatColor.BLUE + "mkc.setrewards " + ChatColor.BLACK + "-" + ChatColor.GREEN
 						+ " /mkc setreward <name of the mob> <kills for reward>");
+				
 			} else if (args[0].equalsIgnoreCase("help")) {
 				sendMKCHelp(sender);
+				
 			} else if (args[0].equalsIgnoreCase("page")) {
 				sender.sendMessage(plugin.getPluginName() + ChatColor.BLUE + " The plugin page is:");
 				sender.sendMessage(ChatColor.GREEN
 						+ "https://www.spigotmc.org/resources/mobkillscounter-rewards-compatible-with-30-mobs-1-7-1-16-perfect-for-survival-servers.82876/");
+			
 			} else if (args[0].equalsIgnoreCase("placeholders")) {
 				if (sender.hasPermission("mkc.placeholders")) {
 					sender.sendMessage(plugin.getPluginName() + ChatColor.GREEN
@@ -122,6 +121,7 @@ public class Comands implements CommandExecutor {
 			} else {
 				sender.sendMessage(plugin.getPluginName() + ChatColor.RED + " This command doesn't exists");
 			}
+			
 		} else {
 			sendMKCHelp(sender);
 		}
